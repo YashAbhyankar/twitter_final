a4c3a3f70de79df70d871419546c421a1958d346
Yash Abhyankar <66163567+YashAbhyankar@users.noreply.github.com>
2020-09-24 17:37:43 +0530

    Update mainbot.py

--- mainbot.py
+++ mainbot.py
@@ -30,9 +30,9 @@ def follower_count(user):
 api = create_api()
 
 while True:
-    user = api.get_user('the_ameen_manna')
-    api.update_profile(name=f'AMEEN|{follower_count(user)} Followers')
-    print(f'Updating Twitter Name : AMEEN|{follower_count(user)} Followers')
+    user = api.get_user('YashAbhyankar5')
+    api.update_profile(name=f'Yash Abhyankar|{follower_count(user)} Followers')
+    print(f'Updating Twitter Name : Yash Abhyankar|{follower_count(user)} Followers')
     print('Waiting to refresh')
     time.sleep(60)
   
